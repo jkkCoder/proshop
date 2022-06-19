5988294b46ac0bbbb2682010363e50dd030312bb
jkkCoder <jayeshk118@gmail.com>
2022-06-19 16:45:01 +0530

    added data from file using seeder.js

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs"
+
+const users = [
+    {
+        name:"Admin user",
+        email:"adming@example.com",
+        password: bcrypt.hashSync("123456",10),
+        isAdmin:true
+    },
+    {
+        name:"jay kumar",
+        email:"jay@example.com",
+        password: bcrypt.hashSync("123456",10),
+    },
+    {
+        name:"golu karn",
+        email:"golu@example.com",
+        password: bcrypt.hashSync("123456",10),
+    },
+]
+
+export default users
