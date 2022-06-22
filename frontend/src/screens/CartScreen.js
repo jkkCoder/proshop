11c7872a2bff1e35ead4a989492d9ad7419bddc5
jkkCoder <jayeshk118@gmail.com>
2022-06-22 07:32:55 +0530

    add to cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React,{useEffect,} from 'react'
+import {useDispatch,useSelector} from "react-redux"
+import Message from "../components/Message"
+import {Link,useNavigate,useParams,useLocation} from "react-router-dom"
+import {Row,Col,ListGroup,Image,Form,Button,Card} from "react-bootstrap"
+import {addToCart} from "../actions/cartAction"
 
 const CartScreen = () => {
+  const productId=useParams().id
+  const navigate = useNavigate()
+  const search = useLocation().search
+
+  const qty = search ? Number(search.split("=")[1]):1
+  const dispatch = useDispatch()
+  const cart = useSelector(state=>state.cart )
+  const {cartItems} = cart
+  console.log(cartItems)
+  
+  useEffect(()=>{
+    if(productId){
+      dispatch(addToCart(productId,qty))
+    }
+  },[dispatch,productId,qty])
+
   return (
     <>
     <div>CartScreen</div>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,7 +8,7 @@ import Loader from "../components/Loader"
 import { listProductDetails } from '../actions/productAction'
 
 const ProductScreen = ({history}) => {
-  const [qty,setQty] = useState(0)
+  const [qty,setQty] = useState(1)
   const id = useParams().id
   const navigate = useNavigate()
 
