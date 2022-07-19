487aa75ee32d70a6ed472608d6390cf1b36db7b3
jkkCoder <jayeshk118@gmail.com>
2022-07-19 08:38:15 +0530

    backend for updating and getting user by id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 const router = express.Router()
-import { authUser,registerUser,getUserProfile,updateUserProfile,getUsers,deleteUser } from "../controllers/userController.js"
+import { getUserById,updateUser,authUser,registerUser,getUserProfile,updateUserProfile,getUsers,deleteUser } from "../controllers/userController.js"
 import {protect,admin} from "../middleware/authMiddleware.js"
 
 router.post("/",registerUser)
@@ -9,6 +9,9 @@ router.post("/login",authUser)
 router.route("/profile")
     .get(protect,getUserProfile)
     .put(protect,updateUserProfile)
-router.route('/:id').delete(protect,admin,deleteUser)
+router.route('/:id')
+    .delete(protect,admin,deleteUser)
+    .get(protect,admin,getUserById)
+    .put(protect,admin,updateUser)
 
 export default router
