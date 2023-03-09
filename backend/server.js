94ec3531b3edb23d83f9b6d199783bc8a585b605
Jayesh Kumar2 <jayeshkumar2@M-XKHM3KV377.local>
2023-03-10 01:45:45 +0530

    added dialogflow backend api

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import path from "path"
-import express from "express"
+import express, { response } from "express"
 import dotenv from "dotenv"
 import morgan from "morgan"
 import connectDb from "./config/db.js"
 import { notFound,errorHandler } from "./middleware/errorMiddleware.js"
+import dialogflow from '@google-cloud/dialogflow';
+import { v4 as uuidv4 } from 'uuid';
 
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
@@ -21,6 +23,49 @@ if(process.env.NODE_ENV === "development"){
 }
 
 app.use(express.json())
+
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS)
+const CONFIGURATION = {
+    credentials: {
+        private_key: CREDENTIALS["private_key"],
+        client_email: CREDENTIALS["client_email"]
+    }
+}
+const sessionClient = new dialogflow.SessionsClient(CONFIGURATION);
+
+// Define a route to handle incoming messages from Dialogflow
+app.post('/dialogflow', async (req, res) => {
+    // Get the message text and session ID from the request body
+    const { message } = req.body;
+    const sessionId = uuidv4()
+
+    console.log("message sent is ",message)
+  
+    // Set up the text query and language code for Dialogflow
+    const request = {
+      session : sessionClient.projectAgentSessionPath(CREDENTIALS['project_id'], sessionId),
+      queryInput: {
+        text: {
+          text: "check my cart",
+          languageCode: 'en-US',
+        },
+      },
+    };
+  
+    try {
+      // Send the text query to Dialogflow and receive a response
+      const responses = await sessionClient.detectIntent(request);
+      const result = responses[0].queryResult;
+
+      console.log("response is ",responses)
+      console.log("result is ",result)
+  
+      res.json({message: result.fulfillmentText})
+    } catch (err) {
+      console.error('ERROR:', err);
+      res.json({ message: 'An error occurred' });
+    }
+  });
 
 app.get("/",(req,res)=>{
     res.send("API IS RUNNING ...")
